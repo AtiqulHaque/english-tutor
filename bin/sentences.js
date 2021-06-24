4000ed844494c55a9664a8148b9d7a3afe286ffb
atiqul <md_atiqulhaque@yahoo.com>
2021-06-24 16:17:06 +0600

    Initialize script

---
+++ bin/sentences.js
@@ -0,0 +1,30 @@
+let sentences = [
+    "The last year i visit Sajek",
+    "I go their with my friend.",
+    "I book two ticket of shanti probohon.",
+    "i start my journy by takeing a cng.",
+    "I reache bus stand.",
+    "i wait a lot after that ",
+    " I find my frind.",
+    "After a log time wait our bus come",
+    " we ride on it.",
+    "we reach khagrachori in the morning at 7 oclock.",
+    "we eat bamboo chcken and That is good",
+    "we go kolank mountain",
+    "The valley is only 8 km far from Mizoram, India",
+    "It is also known as the ‘Roof of Rangamati’ & ‘The Queen of Hills’. ",
+    " As the deep clouds & sudden rain on the valley is the key of attraction, ",
+    "so the rainy season is the best time to travel Sajek Valley.",
+    "Mostly from the beginning of June to the end of September is the best time.",
+    "But you can visit Sajek all around the year.",
+    "Two days & two nights are enough for visiting every attraction & taking the feelings of the valley.",
+    "that time it is raining",
+    "I am waiting for a Riksha",
+    "When i get the riksha suddenly the rain start",
+    "The riksha wala ask me where do i want to go",
+    "Yesterday i tell them i will not be able to met them",
+    "They reply to me that i not able to joint them, let them know"
+    
+]
+
+exports.items = sentences;
